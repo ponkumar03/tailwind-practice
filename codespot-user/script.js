66f59de33779187495573f19ab1b5dc1009c156f
ponkumar <ponkumar@sparkouttech.com>
2023-06-14 21:12:52 +0530

    layer1x events page

---
+++ codespot-user/script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('nav');
+
+      window.addEventListener('scroll',()=>{
+          if(window.scrollY >= 100){
+              nav.classList.add('active_nav');
+          }
+          else{
+              nav.classList.remove('active_nav');
+          }
+      })
