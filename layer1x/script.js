dca9d79cd252b0905a253791969b66feb8dc9392
ponkumar <ponkumar@sparkouttech.com>
2023-06-30 21:20:06 +0530

    layer1x datepicker

--- layer1x/script.js
+++ layer1x/script.js
@@ -65,50 +65,32 @@ window.onload = function() {
   txt.focus();
 }
 
-//  Date-picker
-$(function () {
-  var bindDatePicker = function() {
-   $(".date").datetimepicker({
-       format:'DD MMMM YYYY',
-     icons: {
-       time: "fa fa-clock-o",
-       date: "fa fa-calendar",
-       up: "fa fa-arrow-up",
-       down: "fa fa-arrow-down"
-     }
-   }).find('input:first').on("blur",function () {
-     // check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
-     // update the format if it's yyyy-mm-dd
-     var date = parseDate($(this).val());
+$( function() {
+	$( "#datepicker" ).datepicker({
+		dateFormat: "dd-mm-yy"
+		,	duration: "fast"
+	});
+} );
 
-     if (! isValidDate(date)) {
-       //create date based on momentjs (we have that)
-       date = moment().format('YYYY-MM-DD');
-     }
 
-     $(this).val(date);
-   });
- }
+// Start-Datepicker
+$(document).ready(function(){
+      
+  $(function () {
+  $('#startDate').datepicker({
+   format: 'dd M yyyy' 
+  });
+  });
   
-  var isValidDate = function(value, format) {
-   format = format || false;
-   // lets parse the date to the best of our knowledge
-   if (format) {
-     value = parseDate(value);
-   }
+});
 
-   var timestamp = Date.parse(value);
-
-   return isNaN(timestamp) == false;
-  }
+// END-Datepicker
+$(document).ready(function(){
+      
+  $(function () {
+  $('#endtDate').datepicker({
+   format: 'dd M yyyy' 
+  });
+  });
   
-  var parseDate = function(value) {
-   var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
-   if (m)
-     value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
-
-   return value;
-  }
-  
-  bindDatePicker();
 });
