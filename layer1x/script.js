ca78ce4eb4397a36d536715b347e050f79ef34f6
ponkumar <ponkumar@sparkouttech.com>
2023-07-05 19:11:14 +0530

    user profile tab section

--- layer1x/script.js
+++ layer1x/script.js
@@ -65,13 +65,6 @@ window.onload = function() {
   txt.focus();
 }
 
-$( function() {
-	$( "#datepicker" ).datepicker({
-		dateFormat: "dd-mm-yy"
-		,	duration: "fast"
-	});
-} );
-
 
 // Start-Datepicker
 $(document).ready(function(){
@@ -93,4 +86,27 @@ $(document).ready(function(){
   });
   });
   
+});
+
+
+// Add more field button
+$('.extra-fields-customer').click(function() {
+  $('.customer_records').clone().appendTo('.customer_records_dynamic');
+  $('.customer_records_dynamic .customer_records').addClass('single remove');
+  $('.single .extra-fields-customer').remove();
+  $('.single').append('<a href="#" class="remove-field btn-remove-customer">Remove Fields</a>');
+  $('.customer_records_dynamic > .single').attr("class", "remove");
+
+  $('.customer_records_dynamic input').each(function() {
+    var count = 0;
+    var fieldname = $(this).attr("name");
+    $(this).attr('name', fieldname + count);
+    count++;
+  });
+
+});
+
+$(document).on('click', '.remove-field', function(e) {
+  $(this).parent('.remove').remove();
+  e.preventDefault();
 });
